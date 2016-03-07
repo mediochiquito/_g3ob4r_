b15613c544ff772d2473aa0ce94aa3ed82da448a
mediochiquito <mateo@MacBook-Pro-de-Mateo.local>
2016-03-06 22:22:08 -0300

    pict

--- server/init.php
+++ server/init.php
@@ -12,7 +12,7 @@
 	
 		define("DB_HOST", "localhost");
 		define("DB_USER", "root");
-		define("DB_PASS", "");
+		define("DB_PASS", "root");
 		define("DB_NAME", "geobar");
     
     }else if($_SERVER['HTTP_HOST'] == '192.168.235.140'){
